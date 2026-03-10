d3ea7d19ac0f200657792f2e388539638b19bb3f
kemshim2000 <koyama.kenshin.748@s.kyushu-u.ac.jp>
2026-03-10 22:24:51 +0900

    feat: markdownテーブルのwrapを無効化
    render-markdown.nvim の win_options で wrap=false を設定し、
    LazyVim が markdown FileType に適用する wrap=true を上書きする

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      win_options = {
+        wrap = {
+          default = false,
+          rendered = false,
+        },
+      },
+    },
+  },
+}
